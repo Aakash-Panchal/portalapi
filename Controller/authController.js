b2f9234566698523607415805555681b230727cc
Aakash Panchal <aakashpanchal7000@gmail.com>
2022-11-05 13:18:55 +0530

    fix id pass login

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,64 +1,67 @@
 const Admin = require("../models/adminModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const addAdmin = async (req, res) => {
   try {
-    //Get Admin Details
     const name = req.body.name;
     const Id = req.body.Id;
+    const password = req.body.password;
 
-    //Check If UserName Exist
     const checkUsername = await Admin.findOne({ name });
     if (checkUsername)
       return res.json({ Error: "Username already used", status: false });
 
-    //Check If Email Exist
     const checkId = await Admin.findOne({ Id });
     if (checkId) return res.json({ Error: "Id already used", status: false });
 
-    //Create Model
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const admin = new Admin({
       name: req.body.name,
       Id: req.body.Id,
-      password: req.body.password,
+      password: hashedPassword,
     });
 
-    console.log(admin);
-
-    // Send Data in DataBase
     await admin.save();
 
     res.status(201).send("Admin Added");
   } catch (error) {
-    //Send Error
-    console.log(error);
     res.send(error);
   }
 };
 
 const login = async (req, res) => {
   try {
-    //Get Login Details
     const { Id } = req.body;
+    const password = req.body.password;
 
-    //Find if Admin Exist in Database
     const admin = await Admin.findOne({ Id });
 
     if (!admin)
-      return res.json({ msg: "Incorrect Username or Password", status: false });
+      return res.json({
+        message: "Incorrect Id or Password",
+        status: false,
+        token: null,
+      });
 
-    // Check Password
-    const isPasswordValid = req.body.password;
+    const isPasswordValid = await bcrypt.compare(password, admin.password);
+
+    console.log(isPasswordValid);
+
     if (!isPasswordValid)
-      return res.json({ msg: "Incorrect Username or Password", status: false });
+      return res.json({
+        message: "Incorrect Password",
+        status: false,
+        token: null,
+      });
 
     const token = jwt.sign({ Id }, process.env.JWT_SECRET_KEY, {
       expiresIn: "1d",
     });
 
-    res.json({ message: "Login Success", token: token });
+    res.json({ message: "Login Success", status: true, token: token });
   } catch (error) {
-    //Send Error Message
     res.send("Not authorized");
   }
 };
